30a654c04c20212a346e3b74eeec24bfaee55abd
jayashree100 <sjayashree529@gmail.com>
2022-11-16 12:37:01 +0530

    UC2

--- src/com/bridgelabz/stockaccount/StockAccountManagement.java
+++ src/com/bridgelabz/stockaccount/StockAccountManagement.java
@@ -3,35 +3,44 @@ package com.bridgelabz.stockaccount;
 import java.util.Scanner;
 
 public class StockAccountManagement {
+	
+	final static int MAIN_EXIT = 6;
 
 	public static void main(String[] args) {
 		System.out.println("Stock Account Management Program ! \n");
-		System.out.println("this is first a program");
 		Scanner sc = new Scanner(System.in);
 		StockManager stockManager = new StockManager();
+		AccountTest accountTest = new AccountTest();
 		boolean isExit = false;
 		while (!isExit) {
-			System.out.println("Select the option from below : " + "\n1.Add Stock" + "\n2.print stock report"+ "\n3.Exit program");
+			System.out.println("Select the option from below : " + "\n1.Add Stock" + "\n2.print stock report"+"\n3.Debit Amount"+"\n4.Credit Amount\n"+"5.Check Balance\n"+ MAIN_EXIT+".Exit program");
 			int choice = sc.nextInt();
 
 			switch (choice) {
 			case 1:
 				stockManager.addStock();
 				break;
-
 			case 2:
 				stockManager.printStock();
 				break;
 			case 3:
-				System.out.println("Exit");
+				accountTest.debit();
+				break;
+			case 4:
+				accountTest.credit();
+				break;
+			case 5:
+				accountTest.check_Balance();
+				break;
+			case MAIN_EXIT:
+				System.out.println("Exit : Program Terminated .......");
+				isExit= true;
 				break;
 			default :
 				System.out.println("Kindly Enter a valid option .....");
 			}
-			isExit= false;
 		}
-	}
 
 	}
-
-
+}
+		
